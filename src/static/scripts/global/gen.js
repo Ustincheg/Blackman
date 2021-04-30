5de199bd8aaa9820733b120df4c5cef95ebae878
DMITRIY USTINOV <48724054+Ustincheg@users.noreply.github.com>
2021-04-30 19:05:45 +0300

    modalki

--- src/static/scripts/global/gen.js
+++ src/static/scripts/global/gen.js
@@ -1,7 +1,40 @@
 const showMenu = () => {
-  btn = $(".header__btn-nav-modal");
-  modal = $(".modal-navigation");
+  const btn = $(".header__btn-nav-modal");
   !btn.hasClass("isOpen") ? btn.addClass("isOpen") : btn.removeClass("isOpen");
-  modal.fadeToggle("slow", "linear");
-  console.log(btn);
+  $(".modal-navigation").slideDown();
+  setTimeout(() => {
+    $(".modal-navigation__preloader").fadeToggle("slow", "linear");
+  }, 700);
+};
+
+const hideMenu = () => {
+  const btn = $(".header__btn-nav-modal");
+  $(".modal-navigation__preloader").fadeToggle("slow", "linear");
+  setTimeout(() => {
+    $(".modal-navigation").slideUp();
+  }, 700);
+  setTimeout(() => {
+    !btn.hasClass("isOpen")
+      ? btn.addClass("isOpen")
+      : btn.removeClass("isOpen");
+  }, 1100);
+};
+
+const showModalForm = () => {
+  const modalForm = $(".modal-form");
+  const preloader = $(".modal-form__preloader");
+  $(".modal-form").slideDown();
+  setTimeout(() => {
+    $(".modal-form__button-close").fadeToggle("slow", "linear");
+    $(".modal-form__preloader").fadeToggle("slow", "linear");
+  }, 400);
+};
+
+const hideModalForm = () => {
+  //   $(".modal-form").fadeToggle("slow", "linear");
+  $(".modal-form__preloader").fadeToggle("slow", "linear");
+  $(".modal-form__button-close").fadeToggle("slow", "linear");
+  setTimeout(() => {
+    $(".modal-form").slideUp();
+  }, 400);
 };
