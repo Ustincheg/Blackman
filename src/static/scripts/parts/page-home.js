2d6f21d555924b740dfd2516ba743f0ba4709c3a
GeorgeShadoof <58880355+GeorgiiZhuravlev@users.noreply.github.com>
2021-05-06 00:42:51 +0300

    1.5.1.v.

--- src/static/scripts/parts/page-home.js
+++ src/static/scripts/parts/page-home.js
@@ -24,13 +24,77 @@ $(document).ready(function () {
 //=== CONTENT ===//
 
 $(document).ready(function () {
-  var _target = $('main');
+  if ($('body').hasClass('page-home')) {
+    var _contentArr = $('.main section');
+    var _showCurrentSection = $('.footer-nav-tabs');
+    var _footerInfo = $('.footer-info')[0];
 
-  $(window).on('wheel', function (_evt) {
-    if ($(_target).css('display') === 'none') {
-      $(_target).slideDown(800);
-    } else if ($(_target).scrollTop() == 0 && _evt.originalEvent.deltaY < 0) {
-      $(_target).slideUp(800);
+    $(_showCurrentSection).text($(_contentArr[0]).find('.qs_section-ttl span').text());
+    $(_footerInfo).addClass('_disabled');
+
+    function Open(_currentIndex, _direction) {
+      let _animationTime = 800;
+      if (_currentIndex != undefined && _currentIndex != null) {
+        if (_direction === 'down') {
+          if (_currentIndex != _contentArr.length - 1) {
+            $(_contentArr[_currentIndex + 1]).slideDown(_animationTime, function () {
+              $(_contentArr[_currentIndex]).removeClass('_current');
+              if (_currentIndex + 2 <= _contentArr.length - 1) {
+                $(_showCurrentSection).text($(_contentArr[_currentIndex + 2]).find('.qs_section-ttl span').text());
+              } else {
+                $(_showCurrentSection).addClass('_disabled');
+                $(_footerInfo).removeClass('_disabled');
+              }
+            });
+            $(_contentArr[_currentIndex + 1]).addClass('_opened _current');
+          }
+        } else if (_direction === 'up') {
+          if (_currentIndex != 0) {
+            $(_contentArr[_currentIndex]).slideUp(_animationTime, function () {
+              $(_contentArr[_currentIndex]).removeClass('_opened _current');
+              $(_contentArr[_currentIndex - 1]).addClass('_current');
+              $(_showCurrentSection).removeClass('_disabled');
+              $(_footerInfo).addClass('_disabled');
+              $(_showCurrentSection).text($(_contentArr[_currentIndex]).find('.qs_section-ttl span').text());
+            });
+          } else {
+            $(_contentArr[_currentIndex]).slideUp(_animationTime, function () {
+              $(_contentArr[_currentIndex]).removeClass('_opened _current');
+              $(_showCurrentSection).removeClass('_disabled');
+              $(_footerInfo).addClass('_disabled');
+              $(_showCurrentSection).text($(_contentArr[0]).find('.qs_section-ttl span').text());
+            });
+          }
+        }
+      } else {
+        $(_contentArr[0]).slideDown(_animationTime, function () {
+          $(_contentArr[0]).addClass('_opened _current');
+          $(_showCurrentSection).text($(_contentArr[1]).find('.qs_section-ttl span').text());
+        })
+      }
     }
-  })
+
+    $(window).on('wheel', function (_evt) {
+      let _isAnyCurrent = $('.main section._current');
+
+      if (_isAnyCurrent.length == 0) {
+        Open();
+      } else {
+        let _currentIndex;
+
+        for (let i = 0; i < _contentArr.length; i++) {
+          if ($(_contentArr[i]).hasClass('_current')) {
+            _currentIndex = i;
+            break;
+          }
+        }
+
+        if (_evt.originalEvent.deltaY > 0) {
+          Open(_currentIndex, 'down');
+        } else if (_evt.originalEvent.deltaY < 0) {
+          Open(_currentIndex, 'up');
+        }
+      }
+    })
+  }
 })
