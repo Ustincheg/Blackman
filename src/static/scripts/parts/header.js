626e0c23ab2d1a7336f4a37f4544494a23de9c6e
GeorgeShadoof <58880355+GeorgiiZhuravlev@users.noreply.github.com>
2021-06-01 14:42:28 +0300

    1.7.2.v
    Функция для результата отправки заявки header-callback

--- src/static/scripts/parts/header.js
+++ src/static/scripts/parts/header.js
@@ -133,3 +133,38 @@ $(document).ready(function () {
   //   }
   // })
 })
+
+//=== MODAL REQUEST AJAX RESPONSE ===//
+
+function ModalCallbackAjaxResponse(_status) {
+  var _modal = $('.header-callback');
+
+  switch (_status) {
+    case 'success':
+      $(_modal).find('.header-callback__content').addClass('_disabled');
+      $(_modal).find('.header-callback-success').removeClass('_disabled');
+      var _timeout = setTimeout(function () {
+        $(_modal).find('.header-callback__content').removeClass('_disabled');
+        $(_modal).find('.header-callback-success').addClass('_disabled');
+      }, 3000);
+      $(_modal).find('.header-callback__btn-close').bind('click.timeout', function () {
+        clearTimeout(_timeout);
+        $(_modal).find('.header-callback__content').removeClass('_disabled');
+        $(_modal).find('.header-callback-success').addClass('_disabled');
+      });
+      break;
+    case 'fail':
+      $(_modal).find('.header-callback__content').addClass('_disabled');
+      $(_modal).find('.header-callback-fail').removeClass('_disabled');
+      var _timeout = setTimeout(function () {
+        $(_modal).find('.header-callback__content').removeClass('_disabled');
+        $(_modal).find('.header-callback-fail').addClass('_disabled');
+      }, 3000);
+      $(_modal).find('.header-callback__btn-close').bind('click.timeout', function () {
+        clearTimeout(_timeout);
+        $(_modal).find('.header-callback__content').removeClass('_disabled');
+        $(_modal).find('.header-callback-fail').addClass('_disabled');
+      });
+      break;
+  }
+}
