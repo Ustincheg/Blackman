7c4a06ecaf215e2022cf691b1db9835e8cfa2879
DMITRIY USTINOV <ustinov.net@mail.ru>
2021-11-08 07:40:19 +0300

    Скрипт для хедера

--- src/static/scripts/parts/header.js
+++ src/static/scripts/parts/header.js
@@ -55,4 +55,71 @@ $(document).ready(() => {
       break;
     }
   }
-})
+});
+
+
+//=== HEADER delete LINK ===//
+const $nav = $('.header-nav-list');
+var $btn = $('.header-nav-list button');
+var $vlinks = $('.header-nav-list .visible-links');
+var $hlinks = $('.header-nav-list .hidden-links');
+
+var breaks = [];
+
+function updateNav() {
+  
+  var availableSpace = $btn.hasClass('hidden') ? $nav.width() : $nav.width() - $btn.width() - 30;
+
+  // The visible list is overflowing the nav
+  if($vlinks.width() > availableSpace) {
+
+    // Record the width of the list
+    breaks.push($vlinks.width());
+
+    // Move item to the hidden list
+    $vlinks.children().last().prependTo($hlinks);
+
+    // Show the dropdown btn
+    if($btn.hasClass('hidden')) {
+      $btn.removeClass('hidden');
+    }
+
+  // The visible list is not overflowing
+  } else {
+
+    // There is space for another item in the nav
+    if(availableSpace > breaks[breaks.length-1]) {
+
+      // Move the item to the visible list
+      $hlinks.children().first().appendTo($vlinks);
+      breaks.pop();
+    }
+
+    // Hide the dropdown btn if hidden list is empty
+    if(breaks.length < 1) {
+      $btn.addClass('hidden');
+      $hlinks.addClass('hidden');
+    }
+  }
+
+  // Keep counter updated
+  $btn.attr("count", breaks.length);
+
+  // Recur if the visible list is still overflowing the nav
+  if($vlinks.width() > availableSpace) {
+    updateNav();
+  }
+
+}
+
+// Window listeners
+
+$(window).resize(function() {
+    updateNav();
+});
+
+$btn.on('click', function() {
+  $hlinks.toggleClass('hidden');
+});
+
+updateNav();
