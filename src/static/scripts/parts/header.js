5ff83e3558fb3506de416648b02170da182a7aa9
Ustincheg <ustinov.net@mail.ru>
2022-01-11 17:24:00 +0300

    Добавил удаление элементов меню JS

--- src/static/scripts/parts/header.js
+++ src/static/scripts/parts/header.js
@@ -59,186 +59,48 @@ $(document).ready(() => {
 
 
 // //=== HEADER delete LINK ===//
-// const $nav = $('.header-nav');
-// var $btn = $('.header-nav-list button');
-// var $hlinks = $('.header-nav-list.hidden-links');
-// var $placeHolderWidth = $('.header__btn-nav-modal').width() + $('.header__link-logo').width() + $('.header-wrap').width() + $('.header-contacts').width() + $('.header__btn-callback').width();
-// var $vlinks = $('.header-nav-list');
-
-// var navItemsObj = $('.header-nav-list__item'); 
-// console.log(navItemsObj, "navItems");
-// var navItems = [];
-// var breaks = [];
-// var a = [];
-
-// const disp = (item) => {
-//   console.log(item.length)
-//   for ( var i = 0; i < item.length; i++ ) {
-//     a.push( item[ i ] );
-//     console.log(a);
-//   }  
-// };
-
-// disp(navItemsObj);
-
-
-// function updateNav() {
-
-//   var headerWidhth = $('.header').width();
-//   var displayWidth = document.documentElement.scrollWidth;
-//   var availableSpace = $nav.width();
-//   // var availableSpace = $btn.hasClass('hidden') ? $nav.width() : $nav.width() - $btn.width() - 30;
-
-//   console.log(availableSpace);
-//   console.log($vlinks.width());
-//   console.log(headerWidhth);
-//   console.log(displayWidth);
-
-//   console.log($vlinks.children().last());
-
-//   // The visible list is overflowing the nav
-//   if (headerWidhth < displayWidth) {    
-
-//     // Record the width of the list
-//     breaks.push($vlinks.width());
-//     $vlinks.children()[ breaks.length ].addClass('qs_hidden');
-
-//     console.log(a)
-//     // Move item to the hidden list
-//     console.log($vlinks.children().last());
-//     $vlinks.children().last().prependTo($hlinks);
-//     $vlinks.children().last().addClass('qs_hidden');
-//     $vlinks.children().last().removeClass('header-nav-list__item');
-
-
-//     // The visible list is not overflowing
-//   } else {
-
-//     // There is space for another item in the nav
-//     if (availableSpace > breaks[breaks.length - 1]) {
-
-//       // Move the item to the visible list
-//       $hlinks.children().first().appendTo($vlinks);
-//       breaks.pop();
-//     }
-//   }
-
-//   // Recur if the visible list is still overflowing the nav
-//   if ($vlinks.width() > displayWidth) {
-//     updateNav();    
-//   }
-// }
-
-// // Window listeners
-
-// $(window).resize(function () {
-//   updateNav();  
-// });
-
-// updateNav();
-
-
-
-
-
-
-
-
 
 var $btn = $('.header-nav-list button');
 var $hlinks = $('.header-nav-list.hidden-links');
 var $placeHolderWidth = $('.header__btn-nav-modal').width() + $('.header__link-logo').width() + $('.header-wrap').width() + $('.header-contacts').width() + $('.header__btn-callback').width();
 var $vlinks = $('.header-nav-list');
-
-
-
-// var navItemsObj = $('.header-nav-list__item'); 
-// console.log(navItemsObj, "navItems");
-// var navItems = [];
-// var breaks = [];
-// var a = [];
-
-// const disp = (item) => {
-//   console.log(item.length)
-//   for ( var i = 0; i < item.length; i++ ) {
-//     a.push( item[ i ] );
-//     console.log(a);
-//   }  
-// };
-
-// disp(navItemsObj);
 var displayWidth = document.documentElement.scrollWidth;
-
 const $nav = $('.header-nav');
-
 
 function updateNav() {
 
   var headerWidhth = $('.header').width();
-  var availableSpace = $nav.width();
-  const arrayLinks = $('.header-nav-list').get.reverse();
 
-  
-
-  
-  console.log(availableSpace);
-  console.log($vlinks.width());
-  console.log(headerWidhth);
-  console.log(displayWidth);
-  
-  console.log(typeof($vlinks.children()))
+  // console.log($vlinks.width());
+  // console.log(headerWidhth);
+  // console.log(displayWidth);  
+  // console.log(typeof($vlinks.children()))
 
   if (headerWidhth < displayWidth) {
 
-    // $vlinks.children().each(function (index, element) {
-    //   // index (число) - текущий индекс итерации (цикла)
-    //     // данное значение является числом
-    //     // начинается отсчёт с 0 и заканчивается количеству элементов в текущем наборе минус 1
-    //   // element - содержит DOM-ссылку на текущий элемент
-    //   // if (headerWidhth <= displayWidth) {
-    //   //   break;
-    //   // }
-    //   // console.log('Индекс элемента div: ' + index + '; id элемента = ' + $(element).html()); 
-    //   if (!$(element).hasClass('qs_hidden')) {
-
-    //     $(element).addClass('qs_hidden');
-
-    //   }
-    //   console.log($(element));
-
-    // });
-
     let iRevert = $vlinks.children().length - 1;
-    console.log(iRevert, 'iRevert');
+    // console.log(iRevert, 'iRevert');
 
 
-    for (var i = 0; i <= $vlinks.children().length - 1; i++) {
-      let item = $($vlinks.children()[i]);
+    for (var i = 0; i <= $vlinks.children().length; i++) {
+      // console.log("Ширина хедера",$('.header').width())
+      // console.log("Ширина экрана", document.documentElement.scrollWidth)
+      // console.log(i, "это I");
+      const arr = $vlinks.children();
+
+      let item = $(arr[arr.length - i]);
       if (!item.hasClass('qs_hidden')) {
-
         $(item).addClass('qs_hidden');
-        console.log(item);
+        // console.log(item);
       }
       
       if ($('.header').width() >= document.documentElement.scrollWidth) {
-        console.log('брейк тру')
+        // console.log('брейк тру');
         break;
       }
     }
-
-
-    //     //если размер хедера больше размера 
-    //     $vlinks.children().forEach(element => {
-    //       console.log(element);
-    //     });
   }
 }
-
-// // Recur if the visible list is still overflowing the nav
-// if ($vlinks.width() > displayWidth) {
-//   updateNav();
-// }
-
 
 // Window listeners
 
@@ -247,4 +109,3 @@ $(window).resize(function () {
 });
 
 updateNav();
-console.log("123123")
